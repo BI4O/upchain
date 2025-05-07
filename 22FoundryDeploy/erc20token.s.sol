0269124ab071e8cafcae6b9057983dd8a3a7b134
bi4o <chenjianbiao1995@gmail.com>
2025-05-07 21:40:33 +0800

    foundry1实操

---
+++ 22FoundryDeploy/erc20token.s.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+import {Script, console} from "forge-std/Script.sol";
+import {MyToken} from "../src/erc20token.sol";
+
+contract DeployMyToken is Script {
+    function run() external {
+        // 读取私钥
+        uint256 deployer = vm.envUint("PRIVATE_KEY");
+
+        // 1. 开始广播
+        vm.startBroadcast(deployer);
+
+        // 2. 部署
+        MyToken token = new MyToken("MyToken", "MTK");
+
+        // 3. 停止广播
+        vm.stopBroadcast(); 
+
+        // 4. 打印成功信息
+        console.log("Deployed on chain ID:", block.chainid);
+        console.log("Deployed succsee at:", address(token));
+    }
+}
